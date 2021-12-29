6e334acf7fc3ab109b24e77ad12374337e61ce86
Jake Jones <jakejonesinfo@gmail.com>
2021-12-29 00:29:30 -0500

    responsive

--- src/components/ListView/index.tsx
+++ src/components/ListView/index.tsx
@@ -8,9 +8,10 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Checkbox from "@mui/material/Checkbox";
 import Paper from "@mui/material/Paper";
-import IconButton from '@mui/material/IconButton';
-import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
-import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from "@mui/material/IconButton";
+import IndeterminateCheckBoxIcon from "@mui/icons-material/IndeterminateCheckBox";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Card from '@mui/material/Card';
 
 interface IProps {
   deleteSelectedOrders?: any;
@@ -29,81 +30,109 @@ interface IProps {
 let allChecked = false;
 
 class ListView extends Component<IProps, {}> {
-
   checkAll = (e: any) => {
-    this.props.selectAllOrders(e.target.checked)
-  }
+    this.props.selectAllOrders(e.target.checked);
+  };
 
   deleteSelected = (e: any) => {
-    this.props.deleteSelectedOrders(e.target.checked)
-  }
+    this.props.deleteSelectedOrders(e.target.checked);
+  };
 
   selectOrder = (orderId: number, e: any) => {
-    console.log(e, orderId)
-  }
+    console.log(e, orderId);
+  };
 
   showListActions = () => {
-    const hasChecked =  this.props.orders?.some((order) => {
+    const hasChecked = this.props.orders?.some((order) => {
       return order.isChecked;
-    })
+    });
 
     if (hasChecked) {
       return (
         <IconButton onClick={this.deleteSelected} aria-label="delete">
-          <DeleteIcon sx={{color: '#777'}}/>
+          <DeleteIcon sx={{ color: "#777" }} />
         </IconButton>
-      )
+      );
     }
-  }  
+  };
 
   render() {
     return (
       <>
-      { this.props && this.props.orders ? (
-      <TableContainer component={Paper} sx={{ mt: 2, border: 1, borderColor: "#ccc" }} elevation={0} >
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{width: 100}}>
-                <div className={`orders__list-actions ${this.showListActions() ? 'orders__list-actions-active' : ''}`}>
-                  <Checkbox
-                    checkedIcon={<IndeterminateCheckBoxIcon />}
-                    onChange={this.checkAll}
-                    defaultChecked={allChecked}
-                  />
-                  {this.showListActions()}
-                </div>
-              </TableCell>
-              <TableCell>Order ID</TableCell>
-              <TableCell>Creation Date</TableCell>
-              <TableCell>Created By</TableCell>
-              <TableCell>Order Type</TableCell>
-              <TableCell>Customer</TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            {this.props.orders.map((row) => (
-              <TableRow
-                key={row.orderId}
-                sx={{
-                  "&:last-child td, &:last-child th": { border: 0 },
-                }}
+        {this.props && this.props.orders ? (
+          <div className="orders-table">
+            <div className="orders-table__desktop">
+              <TableContainer
+                component={Paper}
+                sx={{ mt: 2, border: 1, borderColor: "#ccc" }}
+                elevation={0}
               >
-                <TableCell>
-                  <Checkbox onChange={(e) => this.props.selectOrder(row.orderId, e)} checked={row.isChecked || false} />
-                </TableCell>
-                <TableCell>{row.orderId}</TableCell>
-                <TableCell>{row.createdDate}</TableCell>
-                <TableCell>{row.createdByUserName}</TableCell>
-                <TableCell>{row.orderType}</TableCell>
-                <TableCell>{row.customerName}</TableCell>
-              </TableRow>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell sx={{ width: 100 }}>
+                        <div
+                          className={`orders__list-actions ${
+                            this.showListActions()
+                              ? "orders__list-actions-active"
+                              : ""
+                          }`}
+                        >
+                          <Checkbox
+                            checkedIcon={<IndeterminateCheckBoxIcon />}
+                            onChange={this.checkAll}
+                            defaultChecked={allChecked}
+                          />
+                          {this.showListActions()}
+                        </div>
+                      </TableCell>
+                      <TableCell>Order ID</TableCell>
+                      <TableCell>Creation Date</TableCell>
+                      <TableCell>Created By</TableCell>
+                      <TableCell>Order Type</TableCell>
+                      <TableCell>Customer</TableCell>
+                    </TableRow>
+                  </TableHead>
+
+                  <TableBody>
+                    {this.props.orders.map((row) => (
+                      <TableRow
+                        key={row.orderId}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                        selected={row.isChecked}
+                      >
+                        <TableCell>
+                          <Checkbox
+                            onChange={(e) =>
+                              this.props.selectOrder(row.orderId, e)
+                            }
+                            checked={row.isChecked || false}
+                          />
+                        </TableCell>
+                        <TableCell>{row.orderId}</TableCell>
+                        <TableCell>{row.createdDate}</TableCell>
+                        <TableCell>{row.createdByUserName}</TableCell>
+                        <TableCell>{row.orderType}</TableCell>
+                        <TableCell>{row.customerName}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+            <div className="orders-table__mobile">
+            {this.props.orders.map((row) => (
+              <Card elevation={3} sx={{p: 5, my: 2}}>
+                {row.customerName}
+              </Card>
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      ) : ''}
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </>
     );
   }

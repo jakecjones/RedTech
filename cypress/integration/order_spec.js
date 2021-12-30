6137e66ba47b3a3fc72c79a75d832cd812b65047
Jake Jones <jakejonesinfo@gmail.com>
2021-12-30 05:14:47 -0500

    udated testing

--- cypress/integration/order_spec.js
+++ cypress/integration/order_spec.js
@@ -23,6 +23,41 @@ describe('Testing order steps', () => {
         .should('contain', 'automated-business-test')
     })
 
+    it('Can delete all orders', () => {
+        cy.visit(BASE_URL)
+
+        cy.get('.orders__list-actions > .MuiCheckbox-root > .PrivateSwitchBase-input')
+        .click()
+
+        cy.get('[data-testid="DeleteIcon"]')
+        .click()
+
+        cy.get('.no-results')
+
+    })
+
+    it('Can add new order', () => {
+        cy.visit(BASE_URL)
+
+        cy.get('.MuiButton-root')
+        .click()
+
+        cy.get('.mb-1 > .MuiInputBase-root > .MuiInputBase-input')
+        .type('automated-business-test')
+
+        cy.get('.MuiDialogContentText-root > .MuiFormControl-root > .MuiOutlinedInput-root > .MuiSelect-select')
+        .click()
+
+        cy.get('[data-value="SaleOrder"]')
+        .click()
+
+        cy.get('.MuiDialogActions-root > .MuiButton-contained')
+        .click()
+
+        cy.get('.MuiTableBody-root > .MuiTableRow-root > :nth-child(6)')
+        .should('contain', 'automated-business-test')
+    })
+
     it('Can delete single order', () => {
         cy.visit(BASE_URL)
 
